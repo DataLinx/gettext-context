0e4c013c63cad68ca602c343a7e5d3ea60df1a7a
Omer Šabić <omer@datalinx.si>
2023-01-14 19:46:56 +0100

    test: remove needless double include

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,3 @@ require_once __DIR__ .'/../vendor/autoload.php';
 
 // Include our helper functions
 require_once __DIR__ . '/../src/gettext-context.php';
-
-// Include it again to increase code coverage
-include __DIR__ . '/../src/gettext-context.php';
