46036b01f78948dea7279389ff951ec8bf9d36a8
Omer Šabić <omer@datalinx.si>
2023-01-14 18:36:22 +0100

    test: fix codecov if statement coverage

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,6 @@ require_once __DIR__ .'/../vendor/autoload.php';
 
 // Include our helper functions
 require_once __DIR__ . '/../src/gettext-context.php';
+
+// Include it again to increase code coverage
+include __DIR__ . '/../src/gettext-context.php';
